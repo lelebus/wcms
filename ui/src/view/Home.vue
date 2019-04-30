04ed9c7c2792e7c0d2c2e8e08096941c027f1610
Fabio Endrizzi <fabio.endrizzibz@gmail.com>
2019-04-30 17:19:08 +0200

    move frontend into single-repo

---
+++ ui/src/view/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="home">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      message: "Nothing like home"
+    };
+  }
+};
+</script>
+
+<style>
+</style>
