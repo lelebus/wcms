0d3a6942629b1527c2aa7f2d6279e19be85faa07
Fabio Endrizzi <fabio.endrizzibz@gmail.com>
2019-04-30 17:19:08 +0200

    move frontend into single-repo

---
+++ ui/README.md
@@ -0,0 +1,12 @@
+# Prerequisite
+
+- install `yarn`
+- `yarn global add parcel-bundler`
+
+# Developement
+
+`parcel src/index.html`
+
+# Build
+
+`parcel build src/index.html`
---
+++ ui/src/view/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="about">
+    <span>Author: {{ author }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About",
+  data() {
+    return {
+      author: "jcte02"
+    };
+  }
+};
+</script>
+
+<style>
+</style>
