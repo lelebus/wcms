67e2b6400cfa2365703c97004a57fa5b38a6df2a
lelebus <leledecandi@gmail.com>
2019-05-01 18:53:36 +0200

    match sql with structs

--- ER_Schema.sql
+++ ER_Schema.sql
@@ -1,18 +1,19 @@
 CREATE TABLE wine (
   id integer NOT NULL PRIMARY KEY SERIAL UNIQUE,
+  storage-area text NOT NULL,
   type text NOT NULL,
-  storage text NOT NULL,
-  name text NOT NULL,
-  year integer NOT NULL,
-  winery text NOT NULL,
   size float NOT NULL,
-  details text,
+  name text NOT NULL,
+  winery text NOT NULL,
+  year integer NOT NULL,
   territory text,
   region text,
   country text NOT NULL,
   price money NOT NULL,
   catalog text PRIMARY KEY,
-  isActive boolean,
+  details text,
+  internal-notes text,
+  is-active boolean,
   FOREIGN KEY (winery) REFERENCES winery(name) ON DELETE RESTRICT,
   FOREIGN KEY (territoy, region, nation) REFERENCES territory(name, region, nation) ON DELETE RESTRICT,
   FOREIGN KEY (catalog) REFERENCES catalog(id) ON DELETE RESTRICT
@@ -21,26 +22,36 @@ CREATE TABLE wine (
 
 CREATE TABLE winery (
   name text NOT NULL PRIMARY KEY,
-  FOREIGN KEY (name) REFERENCES wine(winery) ON DELETE RESTRICT
 );
 
 
 CREATE TABLE territory (
-  name text NOT NULL PRIMARY KEY,
-  region text NOT NULL PRIMARY KEY,
-  nation text NOT NULL PRIMARY KEY,
-  FOREIGN KEY (name, region, nation) REFERENCES wine(territory, region, nation) ON DELETE RESTRICT
+  name text FOREIGN KEY,
+  region text FOREIGN KEY,
+  nation text NOT NULL FOREIGN KEY,
 );
 
 CREATE TABLE catalog (
-  name text NOT NULL PRIMARY KEY,
-  level integer NOT NULL CHECK(level >= 0 AND level <= 3),
-  FOREIGN KEY (name) REFERENCES wine(catalog)
+  id int NOT NULL PRIMARY KEY,
+  name text NOT NULL,
+  level integer NOT NULL CHECK (level >= 0 AND level <= 3),
+  parent int FOREIGN KEY REFERENCES catalog(id)
+  type text,
+  size float,
+  year int,
+  territory text,
+  region text,
+  country text,
+  winery text,
+  storage text,
+  wine int,
+  FOREIGN KEY (wine) REFERENCES wine(id),
+  FOREIGN KEY (territory, region, country) REFERENCES territory(name, region, country)
 );
 
-
 CREATE TABLE purchase (
-  wine text NOT NULL PRIMARY KEY,
+  id int NOT NULL PRIMARY KEY,
+  wine int NOT NULL PRIMARY KEY,
   date timestamp NOT NULL PRIMARY KEY,
   supplier text,
   quantity integer NOT NULL,
